1c29774e56711680ab58aeef981f30a3db376271
Marcin Dłubakowski <marcin.dlubakowski@htdhealth.com>
2023-12-01 12:00:39 +0100

    Answer to Day 1

--- 1/main.go
+++ 1/main.go
@@ -16,7 +16,22 @@ func main() {
 	data, err := os.ReadFile("input.txt")
 	check(err)
 	lines := strings.Split(string(data), "\n")
+	total := 0
 	for _, line := range lines {
 		fmt.Println(line)
+		first := 0
+		last := 0
+		for _, rune := range line {
+			val := int(rune) - 48 // 48 == 0 in ascii
+			if val >= 1 && val <= 9 {
+				if first == 0 {
+					first = val
+				}
+				last = val
+			}
+		}
+		fmt.Printf("%d, %d\n", first, last)
+		total += first*10 + last // this will evaluate to 0 if there are no digits in line
 	}
+	fmt.Println("Result is: ", total)
 }
