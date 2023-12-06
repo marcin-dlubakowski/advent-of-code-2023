31b9132de1c098c004c379a45f2c3a08469100a6
Marcin Dłubakowski <marcin.dlubakowski@htdhealth.com>
2023-12-06 16:03:27 +0100

    Day 6 Part 2 solution

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -1,21 +1,19 @@
-// static RACES: &[(i32, i32)] = &[(7, 9), (15, 40), (30, 200)];
-static RACES: &[(i32, i32)] = &[(61, 430), (67, 1036), (75, 1307), (71, 1150)];
-
-fn part_one() {
-    let result = RACES
+fn solution(races: &[(i64, i64)]) {
+    let result = races
         .iter()
         .map(|(max_time, record)| {
-            let times: Vec<i32> = (1..*max_time)
+            let times: Vec<i64> = (1..*max_time)
                 .map(|time| time * (max_time - time))
                 .filter(|time| time > record)
                 .collect();
-            times.len() as i32
+            times.len() as i64
         })
-        .product::<i32>();
+        .product::<i64>();
 
     println!("Result is: #{result}");
 }
 
 fn main() {
-    part_one();
+    solution(&[(61, 430), (67, 1036), (75, 1307), (71, 1150)]);
+    solution(&[(61677571, 430103613071150)]);
 }
