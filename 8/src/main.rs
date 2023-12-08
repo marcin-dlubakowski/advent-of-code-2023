8daaeea21d4a24bb2d6cd4e40b3625de7a35f768
Marcin Dłubakowski <marcin.dlubakowski@htdhealth.com>
2023-12-08 23:11:31 +0100

    Day 8 Part 2 solution

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use regex::*;
+use std::collections::HashMap;
 
 type Mapping = HashMap<String, (String, String)>;
 
@@ -31,7 +30,7 @@ fn parse_data() -> (String, Mapping) {
     (instructions, mappings)
 }
 
-fn main() {
+fn part_one() {
     let (instructions, mappings) = parse_data();
     let mut current = String::from("AAA");
     let len = instructions.len();
@@ -53,4 +52,44 @@ fn main() {
         result += 1;
     }
     println!("Result: {result}");
+}
+
+fn part_two() {
+    let (instructions, mappings) = parse_data();
+
+    let re_start = Regex::new(r"\w\wA").unwrap();
+    let re_end = Regex::new(r"\w\wZ").unwrap();
+    let nodes: Vec<&String> = mappings.keys().filter(|x| re_start.is_match(x)).collect();
+
+    let len = instructions.len();
+    let result = nodes
+        .into_iter()
+        .map(|node| {
+            let mut i = 0;
+            let mut result = 0i64;
+            let mut current = node;
+            while !re_end.is_match(current) {
+                let next_move = instructions.chars().nth(i).unwrap();
+                current = match next_move {
+                    'L' => &mappings[current].0,
+                    'R' => &mappings[current].1,
+                    _ => panic!("Unexpected instruction"),
+                };
+                i += 1;
+                if i == len {
+                    i = 0;
+                }
+                result += 1;
+            }
+            println!("Result: {result}");
+            result
+        })
+        .reduce(num::integer::lcm)
+        .unwrap();
+    println!("Result is: {result}");
+}
+
+fn main() {
+    // part_one();
+    part_two();
 }
